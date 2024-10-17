f24f5a5e728704919f26aabf89f4e9a4bf46cf39
Youngdoul <157045613+Youngdoul@users.noreply.github.com>
2024-10-17 12:01:04 -0400

    Update DieFragment.kt

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -5,23 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import kotlin.random.Random
 
-class DieFragment : Fragment() {
+const val DIE_SIDES = "dIcE_SiDeS"
 
-    val DIESIDE = "sidenumber"
+class DiceFragment : Fragment() {
+    private var sides: Int? = null
 
-    lateinit var dieTextView: TextView
-
-    var dieSides: Int = 6
+    // ViewModel declaration
+    private lateinit var viewModel: DiceViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Activity-scoped ViewModel initialization
+        viewModel = ViewModelProvider(requireActivity())[DiceViewModel::class.java]
         arguments?.let {
-            it.getInt(DIESIDE).run {
-                dieSides = this
-            }
+            sides = it.getInt(DIE_SIDES)
         }
     }
 
@@ -29,21 +32,21 @@ class DieFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_die, container, false).apply {
-            dieTextView = findViewById(R.id.dieTextView)
+        return inflater.inflate(R.layout.fragment_dice, container, false).apply {
+            findViewById<Button>(R.id.rollButton).setOnClickListener {
+
+                viewModel.setselectnum( (Random.nextInt(sides!!) + 1))
+            }
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        throwDie()
-        view.setOnClickListener{
-            throwDie()
-        }
-    }
+    companion object {
 
-    fun throwDie() {
-        dieTextView.text = Random.nextInt(dieSides).toString()
+        @JvmStatic
+        fun newInstance(sides: Int) =
+            DiceFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(DIE_SIDES, sides)
+                }
+            }
     }
-}
